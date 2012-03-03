9a960ea0af281414571d73ac428a7ce9ee46a9c2
triggerwoods <keithflint@inbox.ru>
2012-03-03 19:01:08 +0400

    fix controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @event = Event.new
-    @events = Event.latest_events(10)
+   # @events = Event.latest_events(10)
+    @events = Event.all
   end
 end
