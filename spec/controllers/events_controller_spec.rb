ca402f176bdbe96a7b5f63a3f0015764424dfc1f
triggerwoods <keithflint@inbox.ru>
2012-03-22 00:20:28 +0400

    added tests

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,11 +1,25 @@
 require 'spec_helper'
 
 describe EventsController do
+
+
+
   describe 'access controller' do
     it 'should redirect to sign in page' do
       post :create
       response.should redirect_to(new_user_session_path)
     end
+
+    it 'should redirect to sign in page' do
+      delete :destroy
+      response.should redirect_to(new_user_session_path)
+    end
+
+    it 'should redirect to sign in page' do
+      put :update
+      response.should redirect_to(new_user_session_path)
+    end
+
   end
 
 end
